ced41a0eea8d3e6193c7a3397b35da7223ad93f8
nathan <nathanbrakken@gmail.com>
2013-06-12 23:19:21 -0700

    re adds migrations

--- db/migrate/20130613050022_add_values_to_status_and_brand.rb
+++ db/migrate/20130613050022_add_values_to_status_and_brand.rb
@@ -1,1 +1,10 @@
-
+class AddValuesToStatusAndBrand < ActiveRecord::Migration
+  def change
+  	Brand.create :name => 'Sports'
+  	Brand.create :name => 'Originals'
+  	Brand.create :name => 'Outlet'
+  	Status.create :name => 'Possible'
+  	Status.create :name => 'Recent'
+  	Status.create :name => 'Old'
+  end
+end
