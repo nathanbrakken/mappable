40d5c488e934c7b9c3dcd4de1a5f3f33f4ff6355
nathan <nathanbrakken@gmail.com>
2013-06-12 23:15:40 -0700

    removes some migrations

--- db/migrate/20130613050022_add_values_to_status_and_brand.rb
+++ db/migrate/20130613050022_add_values_to_status_and_brand.rb
@@ -1,10 +1,1 @@
-class AddValuesToStatusAndBrand < ActiveRecord::Migration
-  def change
-  	Brand.create :name => 'Sports'
-  	Brand.create :name => 'Originals'
-  	Brand.create :name => 'Outlet'
-  	Status.create :name => 'Possible'
-  	Status.create :name => 'Recent'
-  	Status.create :name => 'Old'
-  end
-end
+
